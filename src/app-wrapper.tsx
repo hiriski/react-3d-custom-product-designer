79f713d19877a4281125f995e0847031e5b81c7c
hiriski <hiriski@outlook.com>
2025-04-13 19:07:43 +0700

    feat: added app layout

--- src/app-wrapper.tsx
+++ src/app-wrapper.tsx
@@ -1,36 +1,23 @@
 // mui
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
 
-// images
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// components
+import Header from './components/header'
+import Designer from './components/designer'
+import Sidebar from './components/sidebar'
 
 const App = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
         height: '100vh',
         width: '100%',
+        backgroundColor: 'background.default',
       }}
     >
-      <Box display='flex' gap={2} mb={1}>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </Box>
-      <Box>
-        <Typography fontWeight='700'>
-          React 3D Custom Product Designer
-        </Typography>
-      </Box>
+      <Header />
+      <Designer />
+      <Sidebar />
     </Box>
   )
 }
