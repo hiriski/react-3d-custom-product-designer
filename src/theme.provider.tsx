2f05914655976b83819c481b0bbef0db445ffe22
hiriski <hiriski@outlook.com>
2025-04-13 18:37:49 +0700

    style: added custom shadows

--- src/theme.provider.tsx
+++ src/theme.provider.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react'
 import {
   Components,
   PaletteOptions,
+  Shadows,
   ThemeOptions,
   ThemeProvider,
   createTheme,
@@ -34,10 +35,22 @@ const theme_palette: PaletteOptions = {
   },
 }
 
+const theme_shadows: Shadows = [
+  'none',
+  '0px 4px 10px 0px rgba(0,0,0,0.1)',
+  '0px 5px 16px 0px rgba(0,0,0,0.1)',
+  '0px 6px 20px 0px rgba(0,0,0,0.1)',
+  '0px 6px 24px 0px rgba(0,0,0,0.1)',
+  '0px 10px 30px 0px rgba(0,0,0,0.1)',
+  '0px 12px 42px 0px rgba(0,0,0,0.1)',
+  '0px 12px 54px 0px rgba(0,0,0,0.1)',
+] as unknown as Shadows
+
 const theme: ThemeOptions = createTheme({
   components: theme_components,
   typography: theme_typography,
   palette: theme_palette,
+  shadows: theme_shadows,
 })
 
 interface Props {
