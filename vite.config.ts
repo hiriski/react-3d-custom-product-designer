0ebb0f09edd73fc33c959ff808302b07ac2477d0
hiriski <hiriski@outlook.com>
2025-06-07 01:31:34 +0700

    chore: added base vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  base: './', 
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
